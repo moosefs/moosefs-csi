361ed03d0e0368ede7cbdd5c87a4d6228e7efa76
anoop <anoop@tuxera.com>
2018-12-28 14:28:21 +0200

    cmd/*, driver/*, scripts/*: Updated license (year)
    1. 2018 -> 2019

--- driver/aws.go
+++ driver/aws.go
@@ -1,5 +1,5 @@
 /*
-   Copyright 2018 Tuxera Oy. All Rights Reserved.
+   Copyright 2019 Tuxera Oy. All Rights Reserved.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
